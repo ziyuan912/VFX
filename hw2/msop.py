3c514768b04e316b2c8a8131cbe4c73859bdc8ba
宋昶松 <b05902106@ntu.edu.tw>
2020-05-04 01:24:21 +0800

    y shift

--- hw2/msop.py
+++ hw2/msop.py
@@ -241,7 +241,8 @@ def multi_band_blending(img1, img2, overlap_w):
 	return result.astype(int)
 
 def ImageMatching(images, matching_pairs, difs):
-	match_image = images[0]
+	padding_images = np.array([cv2.copyMakeBorder(images[i], 50, 50, 0, 0, cv2.BORDER_CONSTANT, value=0) for i in range(images.shape[0])])
+	match_image = padding_images[0]
 	# f = open('match.txt', 'w')
 	for i in range(len(images)):
 		x_allshift = matching_pairs[i][:, 0, 1] - matching_pairs[i][:, 1, 1] + images[i].shape[1]
@@ -276,7 +277,12 @@ def ImageMatching(images, matching_pairs, difs):
 
 		# x_shift = int(images[0].shape[1] + np.mean(matching_pairs[i][:, 0, 1]) - np.mean(matching_pairs[i][:, 1,1]))
 
-		nextimage = images[i+1] if i+1 != len(images) else images[0]
+		nextimage = padding_images[i+1] if i+1 != len(padding_images) else padding_images[0]
+		
+		M = np.float32([[1, 0, 0],[0, 1, -shifts[0]]])
+		match_image=np.asarray(match_image, dtype=np.float32)
+		match_image = cv2.warpAffine(match_image, M, (match_image.shape[1], match_image.shape[0]))
+
 		# match_image = np.concatenate((nextimage[:, :-x_shift//2], match_image[:, x_shift//2:]), 1)
 		match_image = multi_band_blending(nextimage, match_image, shifts[1])
 		# f.write(str(i) + ',' + str(x_shift//2) + '\n')
